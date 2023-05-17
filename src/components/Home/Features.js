ee8dd5ba10383d14ff7d26085e0e4559264f8009
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2023-05-17 17:51:56 +0100

    Up to Features

--- src/components/Home/Features.js
+++ src/components/Home/Features.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { ReactComponent as Gardeners } from "../../assets/gardeners.svg";
+import { ReactComponent as Feature1 } from "../../assets/feature1.svg";
+import { ReactComponent as Feature2 } from "../../assets/feature2.svg";
+import { ReactComponent as Feature3 } from "../../assets/feature3.svg";
+import { BsArrowRight } from "react-icons/bs";
+import { Carousel } from "antd";
 
 function Features() {
   return (
@@ -32,6 +37,57 @@ function Features() {
           </p>
         </div>
         <Gardeners />
+      </div>
+      <div className="mt-[150px]">
+        <div className="flex flex-col items-center text-center pb-[60px]">
+          <h4 className="text-green font-medium ">Menjadi Bagian Bertumbuh</h4>
+          <h2 className="mt-[10px] font-medium text-[54px] w-[65%] text-[#4D4D4D]">
+            Buat Hal Sederhana Jadi Lebih Bermakna
+          </h2>
+          <p className="mt-5 text-xl text-[#656565] w-[55%]">
+            Ciptakan setiap kegiatan jadi lebih bermakna dan bermanfaat untuk
+            kita, dia dan semua
+          </p>
+        </div>
+        <Carousel autoplay className="bg-green rounded-lg pb-20">
+          <div>
+            <div className="mt-[44px] px-10 pt-10 pb-[35px] bg-[#FAFAFA] w-[395px] mx-auto rounded-xl shadow-xl">
+              <Feature1 className="ml-[-22%]" />
+              <p className="mt-5 text-justify text-[#828282] w-[300px]">
+                Anda dapat membantu dengan berdonasi untuk biaya penanaman,
+                perwatan, bibit dan biaya oprasional lainnya
+              </p>
+              <h3 className="mt-[25px] text-green flex items-center">
+                Menjadi Donatur <BsArrowRight className="ml-2" />
+              </h3>
+            </div>
+          </div>
+          <div>
+            <div className="mt-[44px] px-10 py-10 bg-[#FAFAFA] w-[425px] mx-auto rounded-xl shadow-xl">
+              <Feature2 className="ml-[-22%]" />
+              <p className="mt-5 text-justify text-[#828282] w-[330px]">
+                Kamu dapat membantu bertumbuh dengan menjadi relawan kami. Kamu
+                dapat langsung menanam pohon, membantu berkebun, melakukan
+                sosialisasi dan program-program menarik lainnya
+              </p>
+              <h3 className="mt-[22px] text-green flex items-center">
+                Menjadi Relawan <BsArrowRight className="ml-2" />
+              </h3>
+            </div>
+          </div>
+          <div>
+            <div className="mt-[44px] px-10 pt-10 pb-[35px] bg-[#FAFAFA] w-[395px] mx-auto rounded-xl shadow-xl">
+              <Feature3 className="ml-[-22%]" />
+              <p className="mt-5 text-justify text-[#828282] w-[300px]">
+                Kamu akan belajar tentang cara menanam pohon, merawat pohon
+                serta hal lain dengan bergabung sebagai petani
+              </p>
+              <h3 className="mt-[25px] text-green flex items-center">
+                Menjadi Petani <BsArrowRight className="ml-2" />
+              </h3>
+            </div>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
